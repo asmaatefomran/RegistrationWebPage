d431d7e85c6d243337404575b99e587543784e10
Menna Saad <ma90230@gmail.com>
2025-03-23 16:07:09 +0200

    Update

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer>
-    <div class="footer-content">
-        <p>&copy; 2025 My Website. All Rights Reserved.</p>
-        <div class="contact-info">
-            <p><i class="fas fa-envelope"></i> Email: <a href="#">ma90230@gmail.com</a></p>
-            <p><i class="fas fa-phone"></i> Phone: <a href="#">+20 01100772972</a></p>
-        </div>
+<div class="footer-text">
+<p>&copy; 2025 My Website. All Rights Reserved.</p>
+            <div class="footer-links">
+                <a href="#">Terms of Use</a>
+                <a href="#">Privacy Policy</a>
+            </div>
         <div class="social-icons">
             <a href="#"><i class="fab fa-facebook"></i></a>
             <a href="#"><i class="fab fa-linkedin"></i></a>
